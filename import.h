e2b647d2fc9540f3ed0ae5e7da85192bf43e1eea
Alexey Sudachen <alexey@sudachen.name>
2017-01-21 15:45:06 -0300

    updated

--- import.h
+++ import.h
@@ -1,8 +1,32 @@
 
+/*
+ *
+ * Stm32Fx HAL sets 1ms interval for SysTick timer by default,
+ *   please do not change this behaviour!
+ *
+ */
+
+
 #pragma once
 #include <uccm/board.h>
 
 #pragma uccm require(end) += [@inc]/~sudachen/uc_irq/uc_irq.c
+
+typedef enum UcIrqPriority UcIrqPriority;
+
+enum UcIrqPriority
+{
+    UC_HIGH_PRIORITY_IRQ = 0,
+    UC_TIMER_PRIORITY_IRQ,
+    UC_LOW_PRIORITY_IRQ,
+    UC_APP_PRIORITY_IRQ,
+};
+
+#if defined __nRF5x_UC__ || defined __stm32Fx_MCU__
+typedef IRQn_Type UcIrqNo;
+#else
+typedef int UcIrqNo;
+#endif
 
 typedef struct UcIrqHandler UcIrqHandler;
 struct UcIrqHandler
@@ -11,17 +35,15 @@ struct UcIrqHandler
     UcIrqHandler *next;
 };
 
-extern UcIrqHandler uc_irq$Nil;
-#define UC_IRQ_LIST_NIL uc_irq$Nil
-
-#if defined NRF51 || defined NRF52
-extern UcIrqHandler *UC_IRQ_RTC1;
-#define UC_TIMED_IRQ UC_IRQ_RTC1
-#elif defined STM32FX
-extern UcIrqHandler *UC_IRQ_SysTick;
-#define UC_TIMED_IRQ UC_IRQ_SysTick
-#endif
+extern const UcIrqHandler uc_irq$Nil; // place it into ROM
+#define UC_IRQ_LIST_NIL ((UcIrqHandler*)&uc_irq$Nil) // Yep, I know what I do
 
 void ucRegister_IrqHandler(UcIrqHandler *irq, UcIrqHandler **irqList);
 void ucUnregister_IrqHandler(UcIrqHandler *irq, UcIrqHandler **irqList);
 void ucHandle_IRQ(UcIrqHandler *irqList);
+
+extern UcIrqHandler *UC_vIRQ_1ms; // virtual IRQ signalled every 1ms
+#define UC_TIMED_IRQ UC_vIRQ_1ms
+
+void ucRegister_1msHandler(UcIrqHandler *irq);
+void ucUnregister_1msHandler(UcIrqHandler *irq);
--- uc_irq.c
+++ uc_irq.c
@@ -1,42 +1,109 @@
 
 #include <~sudachen/uc_irq/import.h>
 
-UcIrqHandler uc_irq$Nil = { NULL, NULL };
+#ifdef __nRF5x_UC__
+#include <app_util_platform.h>
+#endif
+
+const UcIrqHandler uc_irq$Nil = { NULL, NULL };
 
 void ucHandle_IRQ(UcIrqHandler *irqList)
 {
     UcIrqHandler *irqPtr = irqList;
-    while (irqPtr != &uc_irq$Nil)
+    while (irqPtr != UC_IRQ_LIST_NIL)
     {
-        if ( irqPtr->handler != NULL ) irqPtr->handler(irqPtr);
+        UcIrqHandler *p = irqPtr;
         irqPtr = irqPtr->next;
+        if ( p->handler != NULL ) p->handler(p);
     }
 }
 
-#if defined NRF51 || defined NRF52
-UcIrqHandler *UC_IRQ_RTC1 = &uc_irq$Nil;
+UcIrqHandler *UC_vIRQ_1ms = UC_IRQ_LIST_NIL;
+
+#ifdef __nRF5x_UC__
+
+bool uc_irq$rtcIsStarted = false;
+
 void RTC1_IRQHandler(void)
 {
-    ucHandle_IRQ(UC_IRQ_RTC1);
+    ucHandle_IRQ(UC_TIMED_IRQ);
+    NRF_RTC1->TASKS_CLEAR = 1;
+    __SEV();
 }
-#elif defined STM32FX
-// there is no RTC1 IRQ, only wakeup and alarm
+
 #endif
 
-#if defined NRF51 || defined NRF52
-// there is no SysTick timer
-UcIrqHandler *UC_IRQ_RTC1 = &uc_irq$Nil;
-void RTC1_IRQHandler(void)
+void ucEnable_Irq(UcIrqNo irqNo,UcIrqPriority prio)
 {
-    ucHandle_IRQ(UC_IRQ_RTC1);
+    int realPrio = 15;
+    __Assert(prio >= UC_HIGH_PRIORITY_IRQ && prio <= UC_APP_PRIORITY_IRQ );
+
+#ifdef __nRF5x_UC__
+    switch(prio) {
+        case UC_HIGH_PRIORITY_IRQ: realPrio = APP_IRQ_PRIORITY_HIGH; break;
+        case UC_TIMER_PRIORITY_IRQ:realPrio = APP_IRQ_PRIORITY_MID; break;
+        case UC_LOW_PRIORITY_IRQ:  realPrio = APP_IRQ_PRIORITY_LOW; break;
+        case UC_APP_PRIORITY_IRQ:  realPrio = APP_IRQ_PRIORITY_THREAD; break;
+        default: realPrio = APP_IRQ_PRIORITY_THREAD;
+    }
+#elif defined __stm32Fx_UC__
+    switch(prio) {
+        case UC_HIGH_PRIORITY_IRQ: realPrio  = 0; break;
+        case UC_TIMER_PRIORITY_IRQ:realPrio  = 2; break;
+        case UC_LOW_PRIORITY_IRQ:  realPrio  = 3; break;
+        case UC_APP_PRIORITY_IRQ:  realPrio  = 4; break;
+        default: realPrio = 4;
+    }
+#endif
+
+    NVIC_SetPriority(irqNo,realPrio);
+    NVIC_ClearPendingIRQ(irqNo);
+    NVIC_EnableIRQ(irqNo);
 }
-#elif defined STM32FX
-#pragma uccm cflags+=-DUC_EXTEND_SYSTICK
-// it uses raw SysTick_Handler in HAL
-UcIrqHandler *UC_IRQ_SysTick = &uc_irq$Nil;
-void ucSysTickHandler()
+
+void ucDisable_Irq(UcIrqNo irqNo)
 {
-    ucHandle_IRQ(UC_IRQ_SysTick)
+    NVIC_DisableIRQ(irqNo);
+}
+
+#ifndef SOFTDEVICE_PRESENT
+void uc_irq$requireLfclk()
+{
+    if ( NRF_CLOCK->EVENTS_LFCLKSTARTED )
+    {
+        // ???
+        // setup low frequency clock source here
+        NRF_CLOCK->LFCLKSRC             = (CLOCK_LFCLKSRC_SRC_Synth << CLOCK_LFCLKSRC_SRC_Pos);
+        NRF_CLOCK->EVENTS_LFCLKSTARTED  = 0;
+        NRF_CLOCK->TASKS_LFCLKSTART     = 1;
+        while (NRF_CLOCK->EVENTS_LFCLKSTARTED == 0) (void)0;
+    }
+}
+#endif
+
+#ifdef __nRF5x_UC__
+void uc_irq$startRTC1()
+{
+#ifndef SOFTDEVICE_PRESENT
+    uc_irq$requireLfclk();
+#endif
+    NRF_RTC1->INTENSET = RTC_INTENSET_TICK_Msk;
+    NRF_RTC1->EVTENSET = RTC_EVTENSET_TICK_Msk;
+    NRF_RTC1->TASKS_CLEAR = 1;
+    NRF_RTC1->TASKS_START = 1;
+    ucEnable_Irq(RTC1_IRQn,UC_HIGH_PRIORITY_IRQ);
+}
+
+void uc_irq$stopRTC1()
+{
+    ucDisable_Irq(RTC1_IRQn);
+}
+#endif
+
+#ifdef __stm32Fx_UC__
+// it uses raw SysTick_Handler in HAL
+void ucSysTick1ms()
+    ucHandle_IRQ(UC_TIMED_IRQ)
 }
 #endif
 
@@ -51,10 +118,30 @@ void ucUnregister_IrqHandler(UcIrqHandler *irq, UcIrqHandler **irqList)
 {
     UcIrqHandler **ptr = irqList;
     if ( irq->next == NULL ) return;
-    while ( *ptr != &uc_irq$Nil && *ptr != irq ) ptr = &(*ptr)->next;
-    if ( *ptr != &uc_irq$Nil )
+
+    while ( *ptr != UC_IRQ_LIST_NIL && *ptr != irq ) ptr = &(*ptr)->next;
+
+    if ( *ptr != UC_IRQ_LIST_NIL )
     {
         *ptr = (*ptr)->next;
         irq->next = 0;
     }
+}
+
+void ucRegister_1msHandler(UcIrqHandler *irq)
+{
+    ucRegister_IrqHandler(irq, &UC_vIRQ_1ms);
+#ifdef __nRF5x_UC__
+    if ( UC_vIRQ_1ms != UC_IRQ_LIST_NIL && !uc_irq$rtcIsStarted )
+        uc_irq$startRTC1();
+#endif
+}
+
+void ucUnregister_1msHandler(UcIrqHandler *irq)
+{
+    ucUnregister_IrqHandler(irq, &UC_vIRQ_1ms);
+#ifdef __nRF5x_UC__
+    if ( UC_vIRQ_1ms == UC_IRQ_LIST_NIL && uc_irq$rtcIsStarted )
+        uc_irq$stopRTC1();
+#endif
 }
